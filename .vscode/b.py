9fb76ec7c87017c4948e1e745d1d40c9aa7769c8
Jatin Sharma <js0414030@gmail.com>
2023-07-05 09:15:34 +0530

    here is the commit

---
+++ .vscode/b.py
@@ -0,0 +1,24 @@
+list = ["jatin", "sharma", "harsh", "galve"]
+
+list.extend("jatin")
+print(list)
+
+list.pop(3)
+print(list)
+
+list.remove("sharma")
+print(list)
+
+#list.index(2)
+#p#rint(list)
+
+list[3] = "aakash"
+print(list)
+
+l1 = [1, 23, 44 , 4]
+
+l1.copy()
+print(l1)
+
+list.clear()
+print(list)
